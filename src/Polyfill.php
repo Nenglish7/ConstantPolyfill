6addbd999c52ac70db44d971b577bf31e8ec24cd
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-25 23:55:05 -0500

    Add the class.

--- src/Polyfill.php
+++ src/Polyfill.php
@@ -1,1 +1,20 @@
+<?php
+/**
+ * This file is a part of Nenglish7/ConstantPolyfill.
+ *
+ * @author Nicholas English                        <https://github.com/Nenglish7>.
+ * @author Nenglish7/ConstantPolyfill Contributors <https://github.com/Nenglish7/ConstantPolyfill/graphs/contributors>.
+ *
+ * @copyright 2018 Nicholas English.
+ *
+ * @license <https://github.com/Nenglish7/ConstantPolyfill/blob/master/LICENSE> MIT License.
+ */
 
+namespace Nenglish7\ConstantPolyfill;
+
+/**
+ * Polyfill.
+ */
+class Polyfill extends GetValues
+{
+}
