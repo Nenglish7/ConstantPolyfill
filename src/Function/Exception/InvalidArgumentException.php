2a2c242d0f8a8dab39587275206ed057e8a3144f
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-27 06:19:27 -0500

    Create InvalidArgumentException.php

---
+++ src/Function/Exception/InvalidArgumentException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is a part of Nenglish7/Function.
+ *
+ * Copyright (c) 2018 Nicholas English <https://github.com/Nenglish7>.
+ */
+ 
+namespace Nenglish7\Function\Array;
+
+/**
+ * InvalidArgumentException.
+ */
+ class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+ {
+ }
